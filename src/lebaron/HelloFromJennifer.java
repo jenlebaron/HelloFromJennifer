0f3e49d5080aea1938be919aa31ce7590cd23f1b
jenlebaron <leb20008@byui.edu>
2021-01-28 11:27:26 -0800

    fixed spelling

--- src/lebaron/HelloFromJennifer.java
+++ src/lebaron/HelloFromJennifer.java
@@ -1,7 +1,7 @@
 package lebaron;
 
 public class HelloFromJennifer {
-    public static void main(String[] arge) {
+    public static void main(String[] args) {
         System.out.println("Hello, my name is Jennifer.");
     }
 }
