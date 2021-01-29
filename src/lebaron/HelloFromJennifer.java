4932ebff248222d71525ee124205dfea9838c9f8
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2021-01-28 20:02:31 -0800

    updated with my greeting

--- src/lebaron/HelloFromJennifer.java
+++ src/lebaron/HelloFromJennifer.java
@@ -3,5 +3,6 @@ package lebaron;
 public class HelloFromJennifer {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Jennifer.");
+        System.out.println("Hello Jen, this is Jandy saying hi.");
     }
 }
