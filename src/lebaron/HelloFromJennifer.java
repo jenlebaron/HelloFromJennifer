5cc9ffe2936ac73ec1aa8c1d08351d1f855f09eb
shawnee <shanpan513@gmail.com>
2021-01-29 09:22:40 -0700

    added my change

--- src/lebaron/HelloFromJennifer.java
+++ src/lebaron/HelloFromJennifer.java
@@ -4,5 +4,6 @@ public class HelloFromJennifer {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Jennifer.");
         System.out.println("Hello Jen, this is Jandy saying hi.");
+        System.out.println("Hello Jennifer, my name is Shawnee");
     }
 }
